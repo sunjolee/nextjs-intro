5eda45e884ea07aeaa4936d0138f9a68d9f4b5b5
sunjo <sunjo@prismweaver.com>
2022-07-02 16:41:01 +0900

    2.3

--- nextjs-intro/pages/index.js
+++ nextjs-intro/pages/index.js
@@ -1,49 +1,49 @@
-import Seo from "../components/Seo";
 import { useEffect, useState } from "react";
+import Seo from "../components/Seo";
 
-export default function Home() {
-
-    const [movies, setMovies] = useState();
-    useEffect(() => {
-        (async () => {
-            const { results } = await (await fetch(`/api/movies`)).json();
-            setMovies(results);
-        })();
-    }, []);
-
+export default function Home({ results }) {
     return (
         <div className="container">
-            <Seo title="Home"/>
-            {!movies && <h4>Loading...</h4>}
-            {movies?.map((movie) => (
+            <Seo title="Home" />
+            {results?.map((movie) => (
                 <div className="movie" key={movie.id}>
-                    å<img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
+                    <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
                     <h4>{movie.original_title}</h4>
                 </div>
             ))}
-
             <style jsx>{`
-                .container {
-                  display: grid;
-                  grid-template-columns: 1fr 1fr;
-                  padding: 20px;
-                  gap: 20px;
-                }
-                .movie img {
-                  max-width: 100%;
-                  border-radius: 12px;
-                  transition: transform 0.2s ease-in-out;
-                  box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
-                }
-                .movie:hover img {
-                  transform: scale(1.05) translateY(-10px);
-                }
-                .movie h4 {
-                  font-size: 18px;
-                  text-align: center;
-                }
-          `}</style>
-
+        .container {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          padding: 20px;
+          gap: 20px;
+        }
+        .movie img {
+          max-width: 100%;
+          border-radius: 12px;
+          transition: transform 0.2s ease-in-out;
+          box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+        }
+        .movie:hover img {
+          transform: scale(1.05) translateY(-10px);
+        }
+        .movie h4 {
+          font-size: 18px;
+          text-align: center;
+        }
+      `}</style>
         </div>
     );
+}
+
+export async function getServerSideProps() {
+    const { results } = await (
+        await fetch(`http://localhost:3000/api/movies`)
+    ).json();
+    console.log("호출;")
+    return {
+        props: {
+            results,
+        },
+    };
 }
